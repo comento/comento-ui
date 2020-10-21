9f3af06a56081631ade8567157dbb412898592a2
hanaKim <hananumber1@nate.com>
2020-10-21 11:50:38 +0900

    [add] toast component

--- src/Components/Message/Toast.vue
+++ src/Components/Message/Toast.vue
@@ -1,5 +1,5 @@
 <template>
-	<aside class="c-toast c-toast_container" :class="[computedType]">
+	<aside ref="toast" class="c-toast c-toast_container" :class="[computedType]" :style="motionStyle">
 		<Typography class="c-toast_message" element="p" type="body2">
 			<slot />
 		</Typography>
@@ -19,20 +19,40 @@ export default {
 				return ['information', 'alert', 'success'].indexOf(value) !== -1;
 			},
 		},
+		time: {
+			type: Number,
+			default: 4000,
+		},
+		motion: {
+			type: String,
+			default: 'slideDown',
+		},
+	},
+	data() {
+		return {
+			motionStyle: null,
+		};
 	},
 	computed: {
 		computedType() {
 			return this.type;
 		},
 	},
+	watch: {},
+	mounted() {
+		this.setMotion();
+	},
 	methods: {
-		mapIconNameFromSize(size) {
-			const iconSet = {
-				'x-small': 'IconExclamationSmallFill',
-				small: 'IconExclamationMediumLine',
-				medium: 'IconExclamationLargeLine',
-			};
-			return iconSet[size];
+		setMotion() {
+			const toast = this.$refs.toast;
+			const bottom = toast.getBoundingClientRect().bottom;
+			/*toast의 절대 위치값을 구한 후 -값 만큼 안보이도록 이동한다*/
+			setTimeout(() => {
+				if (this.motion === 'slideDown') {
+					this.motionStyle = toast.style.bottom = -bottom + 'px';
+				}
+			}, this.time);
+
 		},
 	},
 	components: {
@@ -47,12 +67,16 @@ export default {
 	position: fixed;
 	bottom: 16px;
 	left: 50%;
-	transform: translateX(-50%);
 	padding: 0 16px;
+	text-align: center;
+	transform: translateX(-50%);
 	@include border-radius(2px);
+	@include shadow4();
+	@include transition(all 2s ease-in-out);
 	&.information {
 		background: $gray900;
 		color: $white;
+
 	}
 	&.alert {
 		background: $red600;
--- stories/Toast/Toast.stories.js
+++ stories/Toast/Toast.stories.js
@@ -4,9 +4,9 @@ import Toast from '@/src/Components/Message/Toast';
 storiesOf('Toast', module).add('Toast', () => ({
 	components: { Toast },
 	template: `<div>
-    <Toast style="bottom: 80%">한 줄 짜리 Toast 메시지 (type 기본)</Toast>
-    <Toast style="bottom: 60%" type="alert">한 줄 짜리 Toast 메시지 (type alert)</Toast>
-    <Toast style="bottom: 40%" type="success">한 줄 짜리 Toast 메시지 (type success)</Toast>
-  	<Toast style="bottom: 20%">한 줄 짜리 Toast 메시지<br/> 두 줄 짜리 Toast 메시지</Toast>
+    <Toast style="bottom: 50%">한 줄 짜리 Toast 메시지 (type 기본)</Toast>
+    <Toast style="bottom: 25%" type="alert">한 줄 짜리 Toast 메시지 (type alert)</Toast>
+    <Toast type="success">한 줄 짜리 Toast 메시지 - 모션 예시(type success)</Toast>
+  
     </div>`,
 }));
