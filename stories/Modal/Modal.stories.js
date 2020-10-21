702bf6980cc0b2d91a66aad75e5b12c5f09045e1
Donghoon Song <32301380+donghoon-song@users.noreply.github.com>
2020-10-21 14:48:02 +0900

    [change] Modal placeholder props & formValidator props (#44)
    * [change] Modal placeholder props & formValidator props
    
    - placeholder를 props로 변경
    - formValidator를 Function type props로 변경해 유연하게 활용
    
    * [add] Modal storybook
    
    Co-authored-by: GH Pages Bot <hello@ghbot.com>

---
+++ stories/Modal/Modal.stories.js
@@ -0,0 +1,50 @@
+import { storiesOf } from '@storybook/vue';
+import Modal from '@/src/Components/Message/Modal';
+
+storiesOf('Modal', module)
+	.add('Basic Modal', () => ({
+		components: { Modal },
+		template: `<div style="width: 100%;display: flex; justify-content: space-between">
+			<Modal :show="true">
+				<template v-slot:title>
+					Basic modal
+				</template>
+				<template v-slot:contents>
+					본문을 작성하세요.
+				</template>
+				<template v-slot:sub-contents>
+					문단을 분리할 때에는 margin:8px로 구분합니다.
+				</template>
+			</Modal>
+    </div>`,
+	}))
+	.add('WithForm Modal', () => ({
+		components: { Modal },
+		template: `<div style="width: 100%;display: flex; justify-content: space-between">
+			<Modal :show="true" type="withForm" :form-validator="val => val === '제출합니다'">
+				<template v-slot:title>
+					Modal with form
+				</template>
+				<template v-slot:contents>
+					본문을 작성하세요.
+				</template>
+				<template v-slot:sub-contents>
+					문단을 분리할 때에는 margin:8px로 구분합니다.
+				</template>
+			</Modal>
+	</div>`,
+	}))
+	.add('Permission Modal', () => ({
+		components: { Modal },
+		template: `<div style="width: 100%;display: flex; justify-content: space-between">
+			<Modal :show="true" type="permission">
+				<template v-slot:title>
+					Permission modal
+				</template>
+				<template v-slot:contents>
+					본문은 없거나 한 줄만 가능합니다.
+				</template>
+
+			</Modal>
+	</div>`,
+	}));
