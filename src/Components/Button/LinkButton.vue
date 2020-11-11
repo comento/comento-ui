2e1bf02536ffe51e0327856f492265ea2fb34b9b
seongsil <yooo1201@naver.com>
2020-11-11 10:18:47 +0900

    [add] LinkButton, docs

---
+++ src/Components/Button/LinkButton.vue
@@ -0,0 +1,36 @@
+<template>
+	<a class="c-link-button" v-bind="$attrs" :href="to" v-on="$listeners">
+		<Typography :type="type" color="success">
+			<slot />
+		</Typography>
+	</a>
+</template>
+
+<script>
+import Typography from '@/src/Elements/Core/Typography/Typography';
+
+export default {
+	name: 'LinkButton',
+	inheritAttrs: false,
+	props: {
+		type: String,
+		to: {
+			type: String,
+			default: '#none',
+		},
+	},
+	components: { Typography },
+};
+</script>
+
+<style lang="scss" scoped>
+.c-link-button {
+	text-decoration: none;
+
+	&:hover,
+	&:focus {
+		text-decoration: underline;
+		text-decoration-color: $blue600;
+	}
+}
+</style>
--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -1,3 +1,4 @@
 import Button from './Button';
+import LinkButton from './LinkButton';
 
-export { Button };
+export { Button, LinkButton };
