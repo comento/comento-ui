3c061ab67fc992fab9f3489d2f02f4d052488adc
seongsil <yooo1201@naver.com>
2020-11-16 10:51:02 +0900

    [fix] modal export

--- src/Components/Message/index.js
+++ src/Components/Message/index.js
@@ -1,7 +1,7 @@
-import './Modal';
 import Callout from './Callout';
 import Popover from './Popover';
 import Tooltip from './Tooltip';
 import Toast from './Toast';
 
 export { Callout, Popover, Tooltip, Toast };
+export * from './Modal';
