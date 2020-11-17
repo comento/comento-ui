316eb346551109533a8db4f6625a7f952be7a71a
Seongsil Yoo <yooo1201@naver.com>
2020-11-17 14:05:29 +0900

    [change] LinkButton color (#99)

--- src/Components/Button/LinkButton.vue
+++ src/Components/Button/LinkButton.vue
@@ -1,6 +1,6 @@
 <template>
 	<a class="c-link-button" v-bind="$attrs" :href="to" v-on="$listeners">
-		<Typography :type="type" color="success">
+		<Typography :type="type" :color="color">
 			<slot />
 		</Typography>
 	</a>
@@ -20,6 +20,13 @@ export default {
 		to: {
 			type: String,
 			default: 'javascript:void();',
+		},
+		color: {
+			type: String,
+			default: 'blue600',
+			validator(value) {
+				return ['blue600', 'blue400'].indexOf(value) !== -1;
+			},
 		},
 	},
 	components: { Typography },
