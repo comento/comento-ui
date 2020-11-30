4ea8c09a6442261820a860826bf5b30ad5743a45
seongsil <yooo1201@naver.com>
2020-11-30 16:49:08 +0900

    [add] InputGroup

---
+++ src/Components/DataEntry/InputGroup.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="c-input-group">
+		<slot />
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'InputGroup',
+};
+</script>
+
+<style lang="scss" scoped>
+.c-input-group {
+	@include flexbox();
+
+	> :first-child {
+		flex: 1;
+	}
+
+	.c-button {
+		height: 40px;
+	}
+}
+</style>
