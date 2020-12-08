99fd22ab7503aa22c20a518b7dc5d5bfb28b1b74
Donghoon Song <32301380+donghoon-song@users.noreply.github.com>
2020-12-08 12:19:25 +0900

    Feature/add shadow comments (#145)
    * [add] shadow comment
    
    * [add] Shadow story 추가
    
    * [fix] undefined "shadow" issue

---
+++ stories/BoxWithShadow.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="c-application c-box" :class="classes" :style="styles">
+		<slot />
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'BoxWithShadow',
+	props: {
+		shadowType: {
+			type: String,
+			default: '',
+			validator(value) {
+				return ['', 'shadow1', 'shadow2', 'shadow3', 'shadow4'].indexOf(value) !== -1;
+			},
+		},
+	},
+	computed: {
+		classes() {
+			return `c-box--${this.shadowType}`;
+		},
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+.c-box {
+	display: inline-block;
+	padding: 10px;
+	&--shadow1 {
+		@include shadow1();
+	}
+	&--shadow2 {
+		@include shadow2();
+	}
+	&--shadow3 {
+		@include shadow3();
+	}
+	&--shadow4 {
+		@include shadow4();
+	}
+}
+</style>
