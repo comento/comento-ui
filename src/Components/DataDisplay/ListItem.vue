ece74ae8a88d519c542aa1bb0f187ffce67a2333
seongsil <yooo1201@naver.com>
2020-12-09 17:28:09 +0900

    [add] ListItem 개선, cursor props 추가

--- src/Components/DataDisplay/ListItem.vue
+++ src/Components/DataDisplay/ListItem.vue
@@ -1,10 +1,13 @@
 <template>
-	<div class="c-application c-list-item--container" tabindex="0" :class="size" v-on="$listeners">
+	<div class="c-application c-list-item" tabindex="0" :class="[size, computedCursor]" v-on="$listeners">
 		<slot />
 	</div>
 </template>
 
 <script>
+export const ListItemSizes = ['small', 'medium', 'large'];
+export const ListItemCursors = ['pointer', 'default'];
+
 export default {
 	name: 'ListItem',
 	props: {
@@ -12,8 +15,20 @@ export default {
 			type: String,
 			default: 'medium',
 			validator(value) {
-				return ['small', 'medium', 'large'].indexOf(value) !== -1;
+				return ListItemSizes.indexOf(value) !== -1;
 			},
+		},
+		cursor: {
+			type: String,
+			default: 'pointer',
+			validator(value) {
+				return ListItemCursors.indexOf(value) !== -1;
+			},
+		},
+	},
+	computed: {
+		computedCursor() {
+			return `c-${this.cursor}`;
 		},
 	},
 };
@@ -21,22 +36,19 @@ export default {
 
 <style lang="scss" scoped>
 .c-list-item {
-	&--container {
-		padding: 8px 6px;
-		cursor: pointer;
+	padding: 8px 6px;
 
-		&:hover,
-		&:focus {
-			background: $gray100;
-		}
+	&:hover,
+	&:focus {
+		background: $gray100;
+	}
 
-		&.small {
-			padding: 6px 4px;
-		}
+	&.small {
+		padding: 6px 4px;
+	}
 
-		&.large {
-			padding: 12px;
-		}
+	&.large {
+		padding: 12px;
 	}
 }
 </style>
