c3b8d05ed98a7b293696f7813aa6bdd8c7c10f0c
seongsil <yooo1201@naver.com>
2020-12-10 11:51:01 +0900

    [add] NarrowButton size prop

--- src/Components/Button/NarrowButton.vue
+++ src/Components/Button/NarrowButton.vue
@@ -1,6 +1,7 @@
 <template>
 	<button
 		class="c-application c-narrow-button"
+		:class="size"
 		:style="[computedColor]"
 		v-bind="$attrs"
 		type="button"
@@ -13,6 +14,8 @@
 <script>
 import { colors } from '@/src/Elements/Core/Colors';
 
+export const narrowButtonSizes = ['small', 'medium', 'large'];
+
 export default {
 	name: 'NarrowButton',
 	inheritAttrs: false,
@@ -22,6 +25,13 @@ export default {
 			default: 'gray600',
 			validator(value) {
 				return ['gray400', 'gray600'].indexOf(value) !== -1;
+			},
+		},
+		size: {
+			type: String,
+			default: 'small',
+			validator(value) {
+				return narrowButtonSizes.indexOf(value) !== -1;
 			},
 		},
 	},
@@ -41,10 +51,31 @@ export default {
 	cursor: pointer;
 	border: 0;
 	border-radius: 4px;
-	padding: 2px;
 	background: transparent;
-	height: 18px;
-	@include caption1();
+	@include flexbox();
+	@include align-items(center);
+	@include justify-content(center);
+
+	&:hover {
+		background-color: $gray100;
+	}
+
+	// 사이즈
+	&.small {
+		height: 18px;
+		padding: 2px;
+		@include caption1();
+	}
+	&.medium {
+		height: 28px;
+		padding: 4px;
+		@include body2();
+	}
+	&.large {
+		height: 32px;
+		padding: 4px 6px;
+		@include body1();
+	}
 
 	&:hover {
 		background-color: $gray100;
