30a02db5015d71f7cd384d023124b75e4f5f9320
seongsil <yooo1201@naver.com>
2020-12-14 10:06:24 +0900

    [change] 스토리북 컴포넌트들 Meta title 계층 Level2에 맞게 변경

--- stories/Color.stories.js
+++ stories/Color.stories.js
@@ -2,7 +2,7 @@ import { storiesOf } from '@storybook/vue';
 
 import Index from '@/src/Elements/Core/Colors/Index.vue';
 
-storiesOf('Color', module).add('Index', () => ({
+storiesOf('Core/Color', module).add('Index', () => ({
 	components: { Index },
 	template: '<index></index>',
 }));
