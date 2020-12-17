0309d07f9ade92f0cd4581a572779b15d7f10aeb
donghoon-song <thdehdgns@gmail.com>
2020-12-17 15:44:22 +0900

    [add] Swiper 등록

---
+++ src/Components/DataDisplay/Swiper.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>swiper</div>
+</template>
+
+<script>
+export default {
+	name: 'Swiper',
+};
+</script>
+
+<style scoped></style>
--- src/Components/DataDisplay/index.js
+++ src/Components/DataDisplay/index.js
@@ -7,7 +7,8 @@ import ChipGroup from './ChipGroup';
 import Content from './Content';
 import List from './List';
 import ListItem from './ListItem';
+import Swiper from './Swiper';
 import Tab from './Tab';
 
-export { Avatar, Badge, Box, Card, Chip, ChipGroup, Content, List, ListItem, Tab };
+export { Avatar, Badge, Box, Card, Chip, ChipGroup, Content, List, ListItem, Swiper, Tab };
 export * from './Drawer';
