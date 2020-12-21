b1ab0ffdd989ddc9c049cd1cd75fe632e8d001ed
seongsil <yooo1201@naver.com>
2020-12-21 09:43:26 +0900

    [fix] WithFormModal 코드 개선

--- src/Components/Message/Modal/WithFormModal.vue
+++ src/Components/Message/Modal/WithFormModal.vue
@@ -4,10 +4,10 @@
 		:show-close-button="showCloseButton"
 		:max-width="maxWidth"
 		:persistent="persistent"
-		:max-height="isMobile ? '86vh' : '640px'"
+		:max-height="computedMaxHeight"
 		@close="close"
 	>
-		<div class="c-modal--wrapper" :class="computedClass">
+		<div class="c-modal--wrapper" :class="computedClasses">
 			<!-- title 영역 -->
 			<div class="c-modal--title-wrapper">
 				<Typography type="headline5" :align="align" color="gray900">
@@ -86,7 +86,7 @@ export default {
 			type: String,
 			default: '확인',
 			validator(value) {
-				return typeof value === 'boolean';
+				return typeof value === 'string';
 			},
 		},
 		showCloseButton: {
@@ -118,8 +118,17 @@ export default {
 		},
 	},
 	computed: {
-		computedClass() {
-			return [this.scroll && 'scroll', this.showActionButton && 'with-button'];
+		computedClasses() {
+			return [this.computedScroll, this.computedWithButton];
+		},
+		computedScroll() {
+			return { scroll: this.scroll };
+		},
+		computedWithButton() {
+			return { 'with-button': this.showActionButton };
+		},
+		computedMaxHeight() {
+			return this.isMobile ? '86vh' : '640px';
 		},
 	},
 	methods: {
