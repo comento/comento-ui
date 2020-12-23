4dcd8a3bdc028e1fe7644df16933a4609fb53855
hanaKim <hananumber1@nate.com>
2020-12-23 11:01:29 +0900

    [add] divider

--- src/Elements/Utility/Divider.vue
+++ src/Elements/Utility/Divider.vue
@@ -1,39 +1,34 @@
 <template>
 	<hr
-		class="c-divider"
+		class="c-application c-divider"
 		:style="computedStyle"
-		:class="[computedType, computedVertical, computedDirection, computedColor]"
+		:class="[computedType, computedVertical, computedColor]"
 	/>
 </template>
 
 <script>
+import { colors, colorKeys } from '@/src/Elements/Core/Colors';
+export const dividerTypes = ['line', 'dash'];
 export default {
 	name: 'Divider',
 	props: {
-		/* Divider type line, dash */
 		type: {
 			type: String,
 			default: 'line',
+			validator(value) {
+				const isValid = dividerTypes.indexOf(value) !== -1;
+				if (!isValid) {
+					console.error(`${value} is not a valid name of the Divider type`);
+				}
+				return isValid;
+			},
 		},
-		/* Divider 방향
-		 * 가로 (Vertical) : true
-		 * 세로 (Vertical) :false
-		 * */
 		vertical: {
 			type: Boolean,
-			default: true,
-		},
-		/* dividerDirection top, bottom, left, right */
-		direction: {
-			type: String,
-			default: 'top',
-		},
-		/* Divider color
-		 * default :gray200
-		 *  */
-		color: {
-			type: String,
-			default: 'default',
+			default: false,
+			validator(value) {
+				return typeof value === 'boolean';
+			},
 		},
 		width: {
 			type: String,
@@ -43,42 +38,37 @@ export default {
 			type: String,
 			default: '0',
 		},
+		color: {
+			type: String,
+			default: 'gray300',
+			validator(value) {
+				const isValid = colorKeys.indexOf(value) !== -1;
+				if (!isValid) {
+					console.error(`${value} is not a valid name of the typography color`);
+				}
+				return isValid;
+			},
+		},
 	},
 	data: () => ({}),
 	computed: {
 		computedType() {
-			return `c-divider_type_${this.type}`;
+			return this.type;
 		},
 		computedVertical() {
 			if (this.vertical) {
-				return 'c-divider_vertical';
+				return 'vertical';
 			} else {
-				return 'c-divider_horizontal';
+				return 'horizontal';
 			}
-		},
-		computedDirection() {
-			return `c-divider_direction_${this.direction}`;
 		},
 		computedColor() {
-			return `c-divider_color_${this.color}`;
-		},
-		computedWidth() {
-			if (this.vertical) {
-				return this.width;
-			} else {
-				return null;
-			}
-		},
-		computedHeight() {
-			if (!this.vertical) {
-				return this.height;
-			} else {
-				return null;
-			}
+			return this.color && colors[this.color] ? colors[this.color] : 'inherit';
 		},
 		computedStyle() {
 			const { width, height } = this;
 			return {
+				borderColor: this.computedColor,
 				width: `${width}px`,
 				height: `${height}px`,
 			};
@@ -89,50 +79,24 @@ export default {
 
 <style scoped lang="scss">
 /*@import '@/assets/style/base/main';*/
-
 .c-divider {
-	border: 0;
 	margin: 0;
-	&_vertical {
+	border: 0;
+	&.vertical {
+		border-top: 1px;
 		width: 100%;
 		height: 0;
-		border-top: 1px;
 	}
-	&_horizontal {
+	&.horizontal {
+		border-left: 1px;
 		width: 0;
 		height: 100%;
-		border-left: 1px;
-		top: 0;
-		bottom: 0;
-		margin: auto;
 	}
-	&_type_line {
+	&.line {
 		border-style: solid;
 	}
-	&_type_dash {
+	&.dash {
 		border-style: dashed;
-	}
-	&_color_default {
-		border-color: $gray200;
-	}
-	&_color_gray100 {
-		border-color: $gray100;
-	}
-	&_direction_top {
-		top: 0;
-		left: 0;
-	}
-	&_direction_bottom {
-		bottom: 0;
-		left: 0;
-	}
-	&_direction_left {
-		top: 0;
-		left: 0;
-	}
-	&_direction_right {
-		top: 0;
-		right: 0;
 	}
 }
 </style>
