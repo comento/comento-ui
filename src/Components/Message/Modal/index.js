57e4de8ec4d6b60c9bde4f1d564a04aaa3bdddee
HanaKim <hananumber1@nate.com>
2020-12-24 20:47:18 +0900

    [add] alertModal (#221)
    * [add] alertModal 추가
    
    [fix] permissionModal 수정
    [fix] drawer
    
    * [fix]
    
    * [add] alert, permission 피드백 반영
    
    * [fix] overlay z-index
    
    * [change] left -> cancel, right -> confirm으로 변경
    
    * [remove] validator
    
    * [fix] 코드 개선
    
    * [fix] mdx 개선
    
    Co-authored-by: seongsil <yooo1201@naver.com>

--- src/Components/Message/Modal/index.js
+++ src/Components/Message/Modal/index.js
@@ -1,7 +1,8 @@
+import AlertModal from './AlertModal';
 import BasicModal from './BasicModal';
 import FullscreenModal from './FullscreenModal';
 import Modal from './Modal';
 import PermissionModal from './PermissionModal';
 import WithFormModal from './WithFormModal';
 
-export { BasicModal, FullscreenModal, Modal, PermissionModal, WithFormModal };
+export { AlertModal, BasicModal, FullscreenModal, Modal, PermissionModal, WithFormModal };
