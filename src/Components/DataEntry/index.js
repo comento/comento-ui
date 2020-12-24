a91319d2d1e23c933e6fdf3327c71a0907da44f1
HanaKim <hananumber1@nate.com>
2020-12-24 23:43:51 +0900

    [add] form (#229)
    * [add] form
    
    * [add] form
    
    * [fix] form 피드백 반영
    
    * [change] DataEntry 사이 간격
    
    Co-authored-by: donghoon-song <thdehdgns@gmail.com>
    Co-authored-by: Donghoon Song <32301380+donghoon-song@users.noreply.github.com>

--- src/Components/DataEntry/index.js
+++ src/Components/DataEntry/index.js
@@ -1,9 +1,10 @@
 import Checkbox from './Checkbox';
 import Dropdown from './Dropdown';
+import Form from './Form';
 import Input from './Input';
 import InputGroup from './InputGroup';
 import SearchInput from './SearchInput';
 import Select from './Select';
 import TextArea from './TextArea';
 
-export { Checkbox, Dropdown, Input, InputGroup, SearchInput, Select, TextArea };
+export { Checkbox, Dropdown, Form, Input, InputGroup, SearchInput, Select, TextArea };
