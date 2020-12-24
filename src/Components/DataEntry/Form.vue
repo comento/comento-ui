a804b74a7ebf4d00f1d745d0d0ecf33886500285
hanaKim <hananumber1@nate.com>
2020-12-24 12:19:20 +0900

    [add] form

---
+++ src/Components/DataEntry/Form.vue
@@ -0,0 +1,22 @@
+<template>
+	<form class="c-application c-form" v-bind="$attrs" v-on="$listeners">
+		<slot />
+	</form>
+</template>
+
+<script>
+export default {
+	name: 'Form',
+	props: {},
+	data() {},
+	computed: {},
+};
+</script>
+
+<style scoped lang="scss">
+.c-form {
+	&::v-deep .c-application {
+		margin-top: 12px;
+	}
+}
+</style>
