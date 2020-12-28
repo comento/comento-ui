9ef809af4801d7370bec00487089404f4a87d511
donghoon-song <thdehdgns@gmail.com>
2020-12-28 18:16:15 +0900

    [change] FileItem, FileList, FileSummary 이동

--- src/Components/DataEntry/index.js
+++ src/Components/DataEntry/index.js
@@ -2,26 +2,10 @@ import Checkbox from './Checkbox';
 import Dropdown from './Dropdown';
 import Form from './Form';
 import FileInput from './FileInput/FileInput';
-import FileItem from './FileInput/FileItem';
-import FileList from './FileInput/FileList';
-import FileSummary from './FileInput/FileSummary';
 import Input from './Input';
 import InputGroup from './InputGroup';
 import SearchInput from './SearchInput';
 import Select from './Select';
 import TextArea from './TextArea';
 
-export {
-	Checkbox,
-	Dropdown,
-	Form,
-	FileInput,
-	FileItem,
-	FileList,
-	FileSummary,
-	Input,
-	InputGroup,
-	SearchInput,
-	Select,
-	TextArea,
-};
+export { Checkbox, Dropdown, Form, FileInput, Input, InputGroup, SearchInput, Select, TextArea };
