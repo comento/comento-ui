51ccb67d5775c808c40332d31ebbc1bf6cee4718
Donghoon Song <32301380+donghoon-song@users.noreply.github.com>
2020-12-29 11:00:59 +0900

    Feature/add file input (#230)
    * [add] register FileInput
    
    * [add] FileButton
    
    * [add] FileInput
    
    * [add] FileItem
    
    * [add] FileList
    
    * [add] FileInput story
    
    * [change] 필요없는 코드 제거 & story container width 100%
    
    * [remove] 필요없는 파일 제거
    
    * [add] FileSummary
    
    * [add] FileSummary story에 추가
    
    * [add] 컴포넌트들 등록
    
    * [change] FileButton width 100% & cursor pointer & hover style
    
    * [add] add Typography style to FileInput Story
    
    * [fix] 파일등록이슈해결
    
    * [change] FileItem padding
    
    * [change] FileButton 이동
    
    * [change] FileItem, FileList, FileSummary 이동
    
    * [change] File import 경로 변경
    
    * [remove] 기존 파일 제거 & FileInput 이동
    
    * [change] FileButton import 경로 변경
    
    * [change] FileInput 등록 경로 변경
    
    * [remove] 필요없는 props 제거
    
    * [change] FileSummary class명 변경
    
    * [add] accept props 설명추가
    
    * [remove] 불필요한 table값 제거
    
    * [fix] eslint

---
+++ src/Components/Button/FileButton.vue
@@ -0,0 +1,32 @@
+<template>
+	<NarrowButton size="large" @click="$emit('clickFileButton')">
+		<template v-slot:left-icon>
+			<Icon name="IconPlusLargeLine" size="large" color="gray500"></Icon>
+		</template>
+		{{ text }}
+	</NarrowButton>
+</template>
+
+<script>
+import NarrowButton from '@/src/Components/Button/NarrowButton';
+import Icon from '@/src/Elements/Core/Icon/Icon';
+import customValidator from '@/utils/custom-validator.js';
+export default {
+	name: 'FileButton',
+	props: {
+		text: {
+			type: String,
+			default: '파일 첨부하기',
+			validator(value) {
+				return customValidator(value, typeof value === 'string', 'FileButton', 'text');
+			},
+		},
+	},
+	components: {
+		NarrowButton,
+		Icon,
+	},
+};
+</script>
+
+<style scoped></style>
--- src/Components/DataDisplay/index.js
+++ src/Components/DataDisplay/index.js
@@ -5,9 +5,12 @@ import Card from './Card';
 import Chip from './Chip';
 import ChipGroup from './ChipGroup';
 import Content from './Content';
+import FileItem from './FileItem';
+import FileList from './FileList';
+import FileSummary from './FileSummary';
 import List from './List';
 import ListItem from './ListItem';
 import Tab from './Tab';
 
-export { Avatar, Badge, Box, Card, Chip, ChipGroup, Content, List, ListItem, Tab };
+export { Avatar, Badge, Box, Card, Chip, ChipGroup, Content, FileItem, FileList, FileSummary, List, ListItem, Tab };
 export * from './Drawer';
