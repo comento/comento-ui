d4ed1a35b817a92364637804a59282089676a4c3
seongsil <yooo1201@naver.com>
2021-01-04 15:16:51 +0900

    [add] isEmpty global method, badge 빈 값 체크

--- mixins/globalMixin.js
+++ mixins/globalMixin.js
@@ -9,6 +9,19 @@ const globalMixin = {
 				Object.prototype.hasOwnProperty.call(object, propertyName)
 			);
 		},
+		$_isEmpty(value) {
+			return (
+				value === null ||
+				typeof value === 'undefined' ||
+				(typeof value === 'string' && value === '') ||
+				(Array.isArray(value) && value.length < 1) ||
+				(typeof value === 'object' &&
+					value.constructor.name === 'Object' &&
+					Object.keys(value).length < 1 &&
+					Object.getOwnPropertyNames(value) < 1) ||
+				(typeof value === 'object' && value.constructor.name === 'String' && Object.keys(value).length < 1)
+			);
+		},
 	},
 };
 
