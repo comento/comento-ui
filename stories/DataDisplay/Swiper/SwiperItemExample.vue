a522cd11b6bcb87c8ba2c2dbb30ef20cd0133d4c
donghoon-song <thdehdgns@gmail.com>
2021-01-05 10:55:05 +0900

    [add] SwiperItemExample & Story 변경

---
+++ stories/DataDisplay/Swiper/SwiperItemExample.vue
@@ -0,0 +1,24 @@
+<template>
+	<Box :paddings="[100, 32, 100, 32]">
+		<Typography type="display1" color="white">{{ text }}</Typography>
+	</Box>
+</template>
+
+<script>
+import Box from '@/src/Components/DataDisplay/Box';
+import Typography from '@/src/Elements/Core/Typography/Typography';
+export default {
+	name: 'SwiperItemExample',
+	props: {
+		text: {
+			type: String,
+		},
+	},
+	components: {
+		Box,
+		Typography,
+	},
+};
+</script>
+
+<style scoped></style>
