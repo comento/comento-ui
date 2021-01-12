d79023097f6f77a4f0ff66a89d8633d9599645f2
Donghoon Song <32301380+donghoon-song@users.noreply.github.com>
2021-01-12 16:06:23 +0900

    Feature/add alert (#273)
    * [add] Alert
    
    * [add] Alert, Banner 등록
    
    * [add] 줄바꿈 방지 (한줄만 사용)
    
    * [add] Alert story
    
    * [change] icon cursor pointer -> default
    
    * [change] 클래스명 변경

---
+++ src/Components/Message/Alert.vue
@@ -0,0 +1,101 @@
+<template>
+	<div class="c-application c-alert--container" :class="classes">
+		<Grid>
+			<Row class="flex justify-content-between">
+				<StyleCol :col-lg="12" class="flex flex-row justify-content-between">
+					<div class="c-alert--wrapper">
+						<Icon name="IconSecurityLargeFill" :color="computedIconColor" />
+						<Typography class="c-alert--message" color="gray800" type="body2">
+							<slot />
+						</Typography>
+					</div>
+				</StyleCol>
+			</Row>
+		</Grid>
+	</div>
+</template>
+
+<script>
+import Grid from '@/src/Components/Layout/Grid';
+import Row from '@/src/Components/Layout/Row';
+import StyleCol from '@/src/Components/Layout/StyleCol';
+import Icon from '@/src/Elements/Core/Icon/Icon';
+import Typography from '@/src/Elements/Core/Typography/Typography';
+
+export const AlertTypes = ['information', 'warning'];
+
+export default {
+	name: 'Alert',
+	props: {
+		type: {
+			type: String,
+			default: 'information',
+			validator(value) {
+				return AlertTypes.indexOf(value) !== -1;
+			},
+		},
+	},
+	computed: {
+		computedIconColor() {
+			const colorMap = {
+				information: 'gray600',
+				warning: 'red600',
+			};
+			return colorMap[this.type];
+		},
+		classes() {
+			return [`${this.type}`];
+		},
+	},
+	methods: {},
+	components: { Grid, Row, StyleCol, Icon, Typography },
+};
+</script>
+
+<style lang="scss" scoped>
+.c-alert {
+	&--container {
+		position: fixed;
+		top: 0;
+		left: 0;
+		z-index: 10;
+		display: flex;
+		align-items: center;
+		width: 100%;
+		height: $alert-height;
+		white-space: nowrap;
+		svg {
+			flex-shrink: 0;
+			margin-right: 8px;
+			cursor: default;
+		}
+		&::v-deep a {
+			text-decoration: underline !important;
+		}
+		padding: 16px 0;
+
+		// type
+		&.information {
+			background-color: $gray000;
+		}
+		&.warning {
+			background-color: $red000;
+		}
+	}
+	&--wrapper {
+		display: inline-flex;
+		align-items: center;
+	}
+	&--message {
+		width: 100%;
+		word-break: keep-all;
+		&::v-deep strong {
+			@include f-normal();
+		}
+
+		@include pc {
+			width: 100%;
+		}
+	}
+}
+</style>
