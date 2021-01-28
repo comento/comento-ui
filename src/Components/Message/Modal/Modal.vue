30d6ac08ce35dd5a98b0b45466b0884612b39af3
seongsil <yooo1201@naver.com>
2021-01-28 10:22:37 +0900

    [add] PermissionModal content min-height, max-height 추가

--- src/Components/Message/Modal/Modal.vue
+++ src/Components/Message/Modal/Modal.vue
@@ -23,42 +23,33 @@ export default {
 		show: {
 			type: Boolean,
 			default: false,
-			validator(value) {
-				return typeof value === 'boolean';
-			},
 		},
 		showCloseButton: {
 			type: Boolean,
 			default: true,
-			validator(value) {
-				return typeof value === 'boolean';
-			},
 		},
 		maxWidth: {
 			type: String,
-			validator(value) {
-				return typeof value === 'string';
-			},
 		},
 		persistent: {
 			type: Boolean,
 			default: false,
-			validator(value) {
-				return typeof value === 'boolean';
-			},
 		},
 		maxHeight: {
 			type: String,
-			validator(value) {
-				return typeof value === 'string';
-			},
+		},
+		width: {
+			type: String,
+			default: 'auto',
 		},
 	},
 	computed: {
 		computedStyle() {
 			return {
-				'max-width': !this.isMobile && this.maxWidth,
-				'max-height': this.maxHeight,
+				maxWidth: !this.isMobile && this.maxWidth,
+				maxHeight: this.maxHeight,
+				minHeight: this.minHeight,
+				width: this.width,
 			};
 		},
 	},
