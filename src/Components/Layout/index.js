87c064250db4972f6286d290d266c3d285497e16
Donghoon Song <32301380+donghoon-song@users.noreply.github.com>
2021-03-18 11:55:15 +0900

    Feature/register new layout components (#408)
    * [add] NewGrid, NewRow, NewCol 등록
    
    * [fix] 계산식 수정 & offset 추가
    
    * [add] offset story
    
    * [change] NewLayout story -> Layout story에 모으기
    
    * [change] 계산식 반응형으로 해결
    
    * [add] NewGrid story

--- src/Components/Layout/index.js
+++ src/Components/Layout/index.js
@@ -1,5 +1,8 @@
 import Grid from './Grid';
 import Row from './Row';
 import StyleCol from './StyleCol';
+import NewGrid from './NewGrid';
+import NewRow from './NewRow';
+import NewCol from './NewCol';
 
-export { Grid, Row, StyleCol };
+export { Grid, Row, StyleCol, NewGrid, NewRow, NewCol };
--- stories/Layout/Layout.stories.js
+++ stories/Layout/Layout.stories.js
@@ -2,10 +2,15 @@ import { storiesOf } from '@storybook/vue';
 import Grid from '@/src/Components/Layout/Grid';
 import Row from '@/src/Components/Layout/Row';
 import StyleCol from '@/src/Components/Layout/StyleCol';
+
+import NewGrid from '@/src/Components/Layout/NewGrid';
+import NewRow from '@/src/Components/Layout/NewRow';
+import NewCol from '@/src/Components/Layout/NewCol';
+
 import Typography from '@/src/Elements/Core/Typography/Typography';
 
 storiesOf('Layout/Grid', module)
-	.add('Basic', () => ({
+	.add('Basic(Old)', () => ({
 		components: { Grid, Row, StyleCol, Typography },
 		template: `<div>
 						<Typography type="display1" element="div" align="center" class="mb-64">Basic (1140)</Typography>
@@ -47,6 +52,170 @@ storiesOf('Layout/Grid', module)
 							</Row>
 					  </Grid>
 					</div>`,
+	}))
+	.add('Basic(New)', () => ({
+		components: { NewGrid, NewRow, NewCol, Typography },
+		template: `<div>
+		<Typography type="display1" element="div" align="center" >PC (1140)</Typography>
+		<Typography type="display1" element="div" align="center" class="mb-64">Mobile (375)</Typography>
+		<NewGrid>
+			<NewRow>
+				<NewCol :col-sm="4" :col-lg="12" class="mb-40" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-12</Typography>
+				</NewCol>
+			</NewRow>
+			<NewRow>
+				<NewCol :col-sm="3" :col-lg="9" class="mb-40">
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan" >col-9</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="3" class="mb-40">
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-3</Typography>
+				</NewCol>
+			</NewRow>
+			<NewRow>
+				<NewCol :col-sm="1" :col-lg="3" class="mb-40" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-3</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="3" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-3</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="3" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-3</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="3" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-3</Typography>
+				</NewCol>
+			</NewRow>
+			<NewRow>
+				<NewCol :col-sm="1" :col-lg="3" class="mb-40">
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-3</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="3" :offset-lg="3">
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-3</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="3" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-3</Typography>
+				</NewCol>
+			</NewRow>
+			<NewRow>
+				<NewCol :col-sm="1" :col-lg="1" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-1</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="1" :offset-lg="1">
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-1</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="1" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-1</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="1" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-1</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="1" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-1</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="1" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-1</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="1" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-1</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="1" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-1</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="1" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-1</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="1" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-1</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="1" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-1</Typography>
+				</NewCol>
+			</NewRow>
+		</NewGrid>
+		</div>`,
+	}))
+	.add('Full(New)', () => ({
+		components: { NewGrid, NewRow, NewCol, Typography },
+		template: `<div>
+		<Typography type="display1" element="div" align="center" >PC (1140)</Typography>
+		<Typography type="display1" element="div" align="center" class="mb-64">Mobile (375)</Typography>
+		<NewGrid fluid>
+			<NewRow>
+				<NewCol :col-sm="4" :col-lg="12" class="mb-40" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-12</Typography>
+				</NewCol>
+			</NewRow>
+			<NewRow>
+				<NewCol :col-sm="3" :col-lg="9" class="mb-40">
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan" >col-9</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="3" class="mb-40">
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-3</Typography>
+				</NewCol>
+			</NewRow>
+			<NewRow>
+				<NewCol :col-sm="1" :col-lg="3" class="mb-40" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-3</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="3" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-3</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="3" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-3</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="3" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-3</Typography>
+				</NewCol>
+			</NewRow>
+			<NewRow>
+				<NewCol :col-sm="1" :col-lg="3" class="mb-40">
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-3</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="3" :offset-lg="3">
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-3</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="3" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-3</Typography>
+				</NewCol>
+			</NewRow>
+			<NewRow>
+				<NewCol :col-sm="1" :col-lg="1" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-1</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="1" :offset-lg="1">
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-1</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="1" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-1</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="1" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-1</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="1" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-1</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="1" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-1</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="1" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-1</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="1" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-1</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="1" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-1</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="1" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-1</Typography>
+				</NewCol>
+				<NewCol :col-sm="1" :col-lg="1" >
+					<Typography type="headline6" element="p" align="center" class="pa-16" style="background: lightcyan">col-1</Typography>
+				</NewCol>
+			</NewRow>
+		</NewGrid>
+		</div>`,
 	}))
 	.add('Full', () => ({
 		components: { Grid, Row, StyleCol, Typography },
