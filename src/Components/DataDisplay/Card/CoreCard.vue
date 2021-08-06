48f4e13e372fde08962c41261efb25c13ff1eeb3
Seongsil Yoo <yooo1201@naver.com>
2021-08-06 12:03:07 +0900

    [add] CoreCard 개선 (chips slot, props name, docs) (#544)
    * [add] CoreCard chips slot
    
    * [add] CoreCard mdx
    
    * [change] CoreCard props명 변경, count 관련 로직 보완

--- src/Components/DataDisplay/Card/CoreCard.vue
+++ src/Components/DataDisplay/Card/CoreCard.vue
@@ -2,6 +2,7 @@
 	<article class="c-application c-core-card">
 		<Box :paddings="[18, 20, 16, 20]" :has-border="!isMobile" border-color="gray200">
 			<div class="c-core-card--content-wrapper">
+				<slot name="chips" />
 				<Content type="overline">
 					<Typography element="span" v-html="overlineLeft" />
 					· <Typography element="span" v-html="overlineRight" />
@@ -12,14 +13,19 @@
 					<Rating
 						icon-name="IconCommentMediumLine"
 						icon-color="primary"
-						:text="repliesCount"
+						:text="computedRepliesCount"
 						text-color="primary"
 					/>
-					<Rating icon-name="IconLikeMediumLine" icon-color="red400" :text="likeCount" text-color="red600" />
+					<Rating
+						icon-name="IconLikeMediumLine"
+						icon-color="red400"
+						:text="computedLikeCount"
+						text-color="red600"
+					/>
 				</RatingGroup>
 			</div>
 
-			<template v-if="$slots['replies']">
+			<template v-if="$slots['replies'] && showRepliesCount > 0">
 				<Divider type="dash" color="gray200" />
 
 				<div class="c-card--replies-wrapper">
@@ -62,9 +68,9 @@ export default {
 			type: Number,
 			default: 0,
 		},
-		hiddenRepliesCount: {
+		showRepliesCount: {
 			type: Number,
-			default: 0,
+			default: 2,
 		},
 		likeCount: {
 			type: Number,
@@ -73,7 +79,21 @@ export default {
 	},
 	computed: {
 		isShowHiddenRepliesCount() {
-			return this.hiddenRepliesCount > 0;
+			return this.showRepliesCount > 0 && this.repliesCount - this.showRepliesCount > 0;
+		},
+		hiddenRepliesCount() {
+			return this.repliesCount - this.showRepliesCount;
+		},
+		computedRepliesCount() {
+			return this.handleCheckNegativeNumber(this.repliesCount);
+		},
+		computedLikeCount() {
+			return this.handleCheckNegativeNumber(this.likeCount);
+		},
+	},
+	methods: {
+		handleCheckNegativeNumber(value) {
+			return value < 0 ? 0 : value;
 		},
 	},
 	components: {
