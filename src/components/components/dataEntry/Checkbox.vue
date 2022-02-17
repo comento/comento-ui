27713b3113aa191d7a0f869d79eb24e7f54fe8e6
유성실 <yooo1201@naver.com>
2022-02-17 18:04:21 +0900

    refactor: blue600 -> primary

--- src/components/components/dataEntry/Checkbox.vue
+++ src/components/components/dataEntry/Checkbox.vue
@@ -129,7 +129,7 @@ export default {
 		&:focus:not(:disabled) {
 			& + label:before {
 				background-color: $blue100;
-				border-color: $blue600;
+				border-color: $primary;
 			}
 		}
 
@@ -156,7 +156,7 @@ export default {
 			& + label:before {
 				background-color: #fff;
 				background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 18 18' width='16' height='16' fill='%232a7de1'%3E%3Cpath d='M3.75 9.15753L7.5 12.9075L14.25 6.15753L13.1925 5.09253L7.5 10.785L4.8075 8.10003L3.75 9.15753Z'/%3E%3C/svg%3E");
-				border-color: $blue600;
+				border-color: $primary;
 			}
 		}
 
@@ -180,7 +180,7 @@ export default {
 				margin-right: 8px;
 				box-sizing: border-box;
 				&:hover {
-					border-color: $blue600;
+					border-color: $primary;
 					background-color: $blue100;
 				}
 			}
