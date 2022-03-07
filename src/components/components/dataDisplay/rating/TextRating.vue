3ce91652f95151eb0c2895601c5096576eebbe38
donghoon-song <thdehdgns@gmail.com>
2022-03-07 18:09:07 +0900

    feat: TextRating component, story 추가

---
+++ src/components/components/dataDisplay/rating/TextRating.vue
@@ -0,0 +1,41 @@
+<template>
+	<TextRatingText :size="size" class="c-application c-text-rating">
+		<div :class="{ 'mr-4': $slots['text-right'] }">
+			<slot name="text-left" />
+		</div>
+		<div>
+			<slot name="text-right" />
+		</div>
+	</TextRatingText>
+</template>
+
+<script>
+import TextRatingText from '@/components/components/dataDisplay/rating/TextRatingText';
+export const TextRatingSizes = ['default', 'small'];
+
+/**
+ * @displayName c-text-rating
+ */
+export default {
+	name: 'TextRating',
+	props: {
+		size: {
+			type: String,
+			validator(value) {
+				return TextRatingSizes.includes(value);
+			},
+			default: 'default',
+		},
+	},
+	components: {
+		TextRatingText,
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+.c-text-rating {
+	@include flexbox();
+	@include align-items(center);
+}
+</style>
---
+++ src/components/components/dataDisplay/rating/TextRatingText.vue
@@ -0,0 +1,36 @@
+<template>
+	<Typography :type="computedTypoType" :color="computedTypoColor" :font-weight="400">
+		<slot />
+	</Typography>
+</template>
+
+<script>
+import Typography from '@/components/elements/core/typography/Typography';
+
+export default {
+	name: 'TextRatingText',
+	props: {
+		size: {
+			type: String,
+			default: 'default',
+		},
+	},
+	computed: {
+		computedTypoType() {
+			return {
+				default: 'body2',
+				small: 'caption1',
+			}[this.size];
+		},
+		computedTypoColor() {
+			return {
+				default: 'gray700',
+				small: 'gray500',
+			}[this.size];
+		},
+	},
+	components: { Typography },
+};
+</script>
+
+<style lang="scss" scoped></style>
