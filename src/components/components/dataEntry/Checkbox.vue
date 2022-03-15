583864c57c0bcacf6d0a8d4995ed88153352a494
Seongsil Yoo <yooo1201@naver.com>
2022-03-15 22:17:48 +0900

    feat: checkbox 체크 아이콘 변경 (#797)
    * feat: checkbox 체크 아이콘 변경
    
    * feat: 수직중앙정렬 맞추기

--- src/components/components/dataEntry/Checkbox.vue
+++ src/components/components/dataEntry/Checkbox.vue
@@ -148,14 +148,14 @@ export default {
 				background-color: $gray100;
 				border-color: $input-border-color;
 				content: '';
-				background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 18 18' width='16' height='16' fill='%23D2D5D8'%3E%3Cpath d='M3.75 9.15753L7.5 12.9075L14.25 6.15753L13.1925 5.09253L7.5 10.785L4.8075 8.10003L3.75 9.15753Z'/%3E%3C/svg%3E");
+				background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' width='16' height='16' fill='%23cfd4d7'%3E%3Cpath d='M13.8536 4.14645C14.0488 4.34171 14.0488 4.65829 13.8536 4.85355L6.85355 11.8536C6.65829 12.0488 6.34171 12.0488 6.14645 11.8536L2.14645 7.85355C1.95118 7.65829 1.95118 7.34171 2.14645 7.14645C2.34171 6.95118 2.65829 6.95118 2.85355 7.14645L6.5 10.7929L13.1464 4.14645C13.3417 3.95118 13.6583 3.95118 13.8536 4.14645Z'/%3E%3C/svg%3E");
 			}
 		}
 
 		&:checked:not(:disabled) {
 			& + label:before {
 				background-color: #fff;
-				background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 18 18' width='16' height='16' fill='%232a7de1'%3E%3Cpath d='M3.75 9.15753L7.5 12.9075L14.25 6.15753L13.1925 5.09253L7.5 10.785L4.8075 8.10003L3.75 9.15753Z'/%3E%3C/svg%3E");
+				background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' width='16' height='16' fill='%232a7de1'%3E%3Cpath d='M13.8536 4.14645C14.0488 4.34171 14.0488 4.65829 13.8536 4.85355L6.85355 11.8536C6.65829 12.0488 6.34171 12.0488 6.14645 11.8536L2.14645 7.85355C1.95118 7.65829 1.95118 7.34171 2.14645 7.14645C2.34171 6.95118 2.65829 6.95118 2.85355 7.14645L6.5 10.7929L13.1464 4.14645C13.3417 3.95118 13.6583 3.95118 13.8536 4.14645Z'/%3E%3C/svg%3E");
 				border-color: $primary;
 			}
 		}
@@ -174,11 +174,13 @@ export default {
 				content: '';
 				background-color: $gray000;
 				background-position: center center;
+				background-repeat: no-repeat;
 				border: 1px solid $input-border-color;
 				@include border-radius(4px);
 				display: inline-block;
 				margin-right: 8px;
 				box-sizing: border-box;
+				margin-top: 2px;
 				&:hover {
 					border-color: $primary;
 					background-color: $blue100;
@@ -192,18 +194,18 @@ export default {
 			& + label:before {
 				width: 18px;
 				height: 18px;
-				margin-top: 2px;
+				margin-top: 1px;
 				margin-right: 6px;
-				background-size: 16px;
+				background-size: 12px;
 			}
 			&:disabled {
 				&:checked + label:before {
-					background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 18 18' width='12' height='12' fill='%23D2D5D8'%3E%3Cpath d='M3.75 9.15753L7.5 12.9075L14.25 6.15753L13.1925 5.09253L7.5 10.785L4.8075 8.10003L3.75 9.15753Z'/%3E%3C/svg%3E");
+					background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 12 12' width='12' height='12' fill='%23cfd4d7'%3E%3Cpath d='M10.3536 3.14645C10.5488 3.34171 10.5488 3.65829 10.3536 3.85355L5.35355 8.85355C5.15829 9.04882 4.84171 9.04882 4.64645 8.85355L1.64645 5.85355C1.45118 5.65829 1.45118 5.34171 1.64645 5.14645C1.84171 4.95118 2.15829 4.95118 2.35355 5.14645L5 7.79289L9.64645 3.14645C9.84171 2.95118 10.1583 2.95118 10.3536 3.14645Z'/%3E%3C/svg%3E");
 				}
 			}
 			&:checked:not(:disabled) {
 				& + label:before {
-					background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 18 18' width='12' height='12' fill='%232a7de1'%3E%3Cpath d='M3.75 9.15753L7.5 12.9075L14.25 6.15753L13.1925 5.09253L7.5 10.785L4.8075 8.10003L3.75 9.15753Z'/%3E%3C/svg%3E");
+					background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 12 12' width='12' height='12' fill='%232a7de1'%3E%3Cpath d='M10.3536 3.14645C10.5488 3.34171 10.5488 3.65829 10.3536 3.85355L5.35355 8.85355C5.15829 9.04882 4.84171 9.04882 4.64645 8.85355L1.64645 5.85355C1.45118 5.65829 1.45118 5.34171 1.64645 5.14645C1.84171 4.95118 2.15829 4.95118 2.35355 5.14645L5 7.79289L9.64645 3.14645C9.84171 2.95118 10.1583 2.95118 10.3536 3.14645Z'/%3E%3C/svg%3E");
 				}
 			}
 		}
@@ -212,25 +214,22 @@ export default {
 	&.xsmall {
 		input[type='checkbox'] {
 			& + label {
-				> div {
-					margin-top: 2px;
-				}
 				&:before {
 					margin-top: 0;
 					margin-right: 4px;
 					width: 16px;
 					height: 16px;
-					background-size: 14px;
+					background-size: 12px;
 				}
 			}
 			&:disabled {
 				&:checked + label:before {
-					background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 18 18' width='12' height='12' fill='%23D2D5D8'%3E%3Cpath d='M3.75 9.15753L7.5 12.9075L14.25 6.15753L13.1925 5.09253L7.5 10.785L4.8075 8.10003L3.75 9.15753Z'/%3E%3C/svg%3E");
+					background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 12 12' width='12' height='12' fill='%23cfd4d7'%3E%3Cpath d='M10.3536 3.14645C10.5488 3.34171 10.5488 3.65829 10.3536 3.85355L5.35355 8.85355C5.15829 9.04882 4.84171 9.04882 4.64645 8.85355L1.64645 5.85355C1.45118 5.65829 1.45118 5.34171 1.64645 5.14645C1.84171 4.95118 2.15829 4.95118 2.35355 5.14645L5 7.79289L9.64645 3.14645C9.84171 2.95118 10.1583 2.95118 10.3536 3.14645Z'/%3E%3C/svg%3E");
 				}
 			}
 			&:checked:not(:disabled) {
 				& + label:before {
-					background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 18 18' width='12' height='12' fill='%232a7de1'%3E%3Cpath d='M3.75 9.15753L7.5 12.9075L14.25 6.15753L13.1925 5.09253L7.5 10.785L4.8075 8.10003L3.75 9.15753Z'/%3E%3C/svg%3E");
+					background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 12 12' width='12' height='12' fill='%232a7de1'%3E%3Cpath d=' M10.3536 3.14645C10.5488 3.34171 10.5488 3.65829 10.3536 3.85355L5.35355 8.85355C5.15829 9.04882 4.84171 9.04882 4.64645 8.85355L1.64645 5.85355C1.45118 5.65829 1.45118 5.34171 1.64645 5.14645C1.84171 4.95118 2.15829 4.95118 2.35355 5.14645L5 7.79289L9.64645 3.14645C9.84171 2.95118 10.1583 2.95118 10.3536 3.14645Z'/%3E%3C/svg%3E");
 				}
 			}
 		}
