c0a3426240839feec886c55b3dc24bb482f0729b
Donghoon Song <32301380+donghoon-song@users.noreply.github.com>
2022-11-16 12:04:55 +0900

    feat: UserInformation component 변경 (#948)
    * feat: UserInformation component 변경
    
    UserInformation 에서는 slot 자리만 만들어놓고, 나머지 세부 컴포넌트는 따로 만들어서 자리에 배치하는 식으로 변경. 이렇게 하면 변화에 더 유연하지 않을까 생각이 듭니다.
    
    * fix: style 문법 수정
    
    -
    
    * fix: 질문자 story 예시에 UserInformationName 추가
    
    -
    
    * fix: UI 수정 (정렬, 간격)
    
    -
    
    * fix: 정의되지 않은 변수 제거
    
    -
    
    * fix: UserInformationName type, font-weight 추가
    
    -
    
    * fix: UserInformation avatar wrapper margin 수정
    
    -
    
    * fix: UserInformationName ellipsis 적용
    
    -
    
    * feat: UserInformationCompany 모바일 글자수 제한
    
    -
    
    * fix: UserInformation 재료 컴포넌트들 등록
    
    -
    
    * chore: 주석 추가
    
    -

--- src/components/components/dataDisplay/content/index.js
+++ src/components/components/dataDisplay/content/index.js
@@ -1,5 +1,17 @@
 import Content from './Content';
 import ProfileWithText from './ProfileWithText';
 import UserInformation from './UserInformation';
+import UserInformationName from './UserInformationName';
+import UserInformationCaption from './UserInformationCaption';
+import UserInformationChipGroup from './UserInformationChipGroup';
+import UserInformationCompany from './UserInformationCompany';
 
-export { Content, ProfileWithText, UserInformation };
+export {
+	Content,
+	ProfileWithText,
+	UserInformation,
+	UserInformationName,
+	UserInformationCaption,
+	UserInformationChipGroup,
+	UserInformationCompany,
+};
