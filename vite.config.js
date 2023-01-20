f391850174492d89185af6bcabfc68633bd3413b
donghoon-song <thdehdgns@gmail.com>
2023-01-20 16:38:07 +0900

    chore: vite.config.js 추가
    -
    - @ alias 추가
    - 전역 scss 파일 추가

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import { createVuePlugin as vue } from 'vite-plugin-vue2';
+const path = require('path');
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: `@import "@/assets/style/base/main";`,
+			},
+		},
+	},
+});
