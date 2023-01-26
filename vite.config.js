0fbe2ca89df7d8c9f1092e150ed8a13d1450401c
donghoon-song <thdehdgns@gmail.com>
2023-01-26 17:57:36 +0900

    refactor: path.resolve -> resolve
    -

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import { createVuePlugin as vue } from 'vite-plugin-vue2';
-const path = require('path');
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
 	resolve: {
 		alias: {
-			'@': path.resolve(__dirname, './src'),
+			'@': resolve(__dirname, './src'),
 		},
 	},
 	css: {
