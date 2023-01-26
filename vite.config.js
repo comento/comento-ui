bf465db90b1c7fdb14a9944a344ea7b190cb616c
donghoon-song <thdehdgns@gmail.com>
2023-01-26 18:08:44 +0900

    feat: vite lib build config 추가 & entry 추가 & pcakge.json 설정
    -

--- vite.config.js
+++ vite.config.js
@@ -17,4 +17,25 @@ export default defineConfig({
 			},
 		},
 	},
+	build: {
+		lib: {
+			// Could also be a dictionary or array of multiple entry points
+			entry: resolve(__dirname, 'lib/main.js'),
+			name: 'ComentoUi',
+			// the proper extensions will be added
+			fileName: format => `comento-ui.${format}.js`,
+		},
+		rollupOptions: {
+			// make sure to externalize deps that shouldn't be bundled
+			// into your library
+			external: ['vue'],
+			output: {
+				// Provide global variables to use in the UMD build
+				// for externalized deps
+				globals: {
+					vue: 'Vue',
+				},
+			},
+		},
+	},
 });
