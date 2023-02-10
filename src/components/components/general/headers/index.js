c6d328403da88b48b5220263f8c04e1dd451a710
MINGI KIM <tkg78912@gmail.com>
2023-02-10 11:46:43 +0900

    chore: AutocompleteItem 제거

--- src/components/components/general/headers/index.js
+++ src/components/components/general/headers/index.js
@@ -2,6 +2,5 @@ import SubHeader from './SubHeader';
 import SearchTopBar from './SearchTopBar';
 import TopBar from './TopBar';
 import Header from './Header';
-import AutocompleteItem from './AutocompleteItem';
 
-export { SubHeader, SearchTopBar, TopBar, Header, AutocompleteItem };
+export { SubHeader, SearchTopBar, TopBar, Header };
