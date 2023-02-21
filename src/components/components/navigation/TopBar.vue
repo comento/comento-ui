ebdf6ff7ef6565af84322d7eaa60895ae6ee4444
MINGI KIM <tkg78912@gmail.com>
2023-02-21 17:06:01 +0900

    feat: top bar 변경

--- src/components/components/navigation/TopBar.vue
+++ src/components/components/navigation/TopBar.vue
@@ -5,7 +5,18 @@
 				<NewCol :col-sm="12">
 					<!-- 공통 -->
 					<div class="c-top-bar-container">
-						<div class="preload-image-container">
+						<div v-if="type === 'detail'" class="c-top-bar-backward">
+							<Icon
+								name="IconBackwardLargeLine"
+								color="gray800"
+								class="mr-4 c-pointer"
+								@click="$emit('click-backward')"
+							/>
+							<Typography type="body1" :font-weight="500" color="gray900">
+								{{ title }}
+							</Typography>
+						</div>
+						<div v-else class="preload-image-container">
 							<Logo
 								name="LogoComentoTypoType"
 								:color="isTransparent ? 'white' : 'primary'"
@@ -30,6 +41,9 @@ import NewGrid from '@/components/layout/NewGrid';
 import NewRow from '@/components/layout/NewRow';
 import NewCol from '@/components/layout/NewCol';
 import Logo from '@/components/elements/core/logo/Logo';
+import Icon from '@/components/elements/core/icon/Icon';
+import Typography from '@/components/elements/core/typography/Typography';
+export const headerTypes = ['default', 'detail'];
 
 /**
  * @displayName c-top-bar
@@ -42,8 +56,16 @@ export default {
 			type: Boolean,
 			default: false,
 		},
+		type: {
+			type: String,
+			default: 'default',
+		},
+		title: {
+			type: String,
+			default: '',
+		},
 	},
-	components: { NewCol, NewRow, NewGrid, Logo },
+	components: { NewCol, NewRow, NewGrid, Logo, Icon, Typography },
 };
 </script>
 
@@ -65,6 +87,12 @@ export default {
 
 	&.transparent {
 		background-color: transparent;
+	}
+
+	&-backward {
+		@include flexbox();
+		@include justify-content(center);
+		@include align-items(center);
 	}
 
 	&-content {
