451a60898776cfa1c0206ba613e8198beaa6645e
Seongsil Yoo <yooo1201@naver.com>
2023-05-08 14:39:52 +0900

    feat: Header 내 Tooltip 관련 코드 제거 (#1079)

--- src/components/components/navigation/Header.vue
+++ src/components/components/navigation/Header.vue
@@ -19,19 +19,6 @@
 			>
 				<nuxt-link v-if="nuxt" v-bind="$attrs" :to="menu.path" v-on="$listeners">{{ menu.name }}</nuxt-link>
 				<a v-else v-bind="$attrs" :href="menu.path" v-on="$listeners">{{ menu.name }}</a>
-				<template v-if="menu.name === '클래스' && !hideClassTooltip">
-					<Tooltip open placement="bottom-left">
-						<template v-slot:popover>
-							<div class="flex flex-row align-center justify-between">
-								<Typography type="caption1" color="white">
-									5주 직무경험, 직무부트캠프는<br />
-									클래스에서 만날 수 있어요!
-								</Typography>
-								<Icon name="IconCloseSmallLine" color="white" class="ml-8" @click="closeClassToolTip" />
-							</div>
-						</template>
-					</Tooltip>
-				</template>
 			</li>
 		</ul>
 	</div>
@@ -39,9 +26,6 @@
 
 <script>
 import Logo from '@/components/elements/core/logo/Logo';
-import Tooltip from '@/components/components/message/Tooltip.vue';
-import Icon from '@/components/elements/core/icon/Icon.vue';
-import Typography from '@/components/elements/core/typography/Typography.vue';
 
 /**
  * @displayName c-header
@@ -75,14 +59,6 @@ export default {
 			default: 'https://comento.kr',
 		},
 	},
-	data() {
-		return {
-			hideClassTooltip: false,
-		};
-	},
-	mounted() {
-		this.hideClassTooltip = localStorage.getItem('close-comento-class-tooltip');
-	},
 	methods: {
 		getHeaderNavClass(name) {
 			return {
@@ -94,12 +70,8 @@ export default {
 		onMouseEntered(index) {
 			this.$emit('update-nav', index);
 		},
-		closeClassToolTip() {
-			this.hideClassTooltip = true;
-			localStorage.setItem('close-comento-class-tooltip', true);
-		},
 	},
-	components: { Typography, Icon, Tooltip, Logo },
+	components: { Logo },
 };
 </script>
 
@@ -143,17 +115,6 @@ export default {
 
 		&:not(:first-child) {
 			margin-left: 24px;
-		}
-	}
-	::v-deep {
-		.c-tooltip {
-			left: 20px !important;
-			&--inner {
-				background: $primary;
-			}
-			&--arrow {
-				border-color: $primary;
-			}
 		}
 	}
 }
