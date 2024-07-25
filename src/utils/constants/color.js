8dd4d09a8c0a46eb869c545298553c7d6c422668
LuiGeeDev <45070463+LuiGeeDev@users.noreply.github.com>
2024-07-25 18:35:59 +0900

    feat: 050 컬러 추가 (#1157)
    * feat: 050 컬러 추가
    
    * fix: HEX 코드 소문자로 변경

--- src/utils/constants/color.js
+++ src/utils/constants/color.js
@@ -4,57 +4,62 @@
 export const black = '#000000';
 export const gray900 = '#040505';
 export const gray850 = '#202325';
-export const gray800 = '#3C4144';
-export const gray700 = '#595F63';
-export const gray600 = '#767D82';
-export const gray500 = '#949BA0';
-export const gray400 = '#ADB3B8';
-export const gray300 = '#CFD4D7';
-export const gray250 = '#E1E4E6';
-export const gray200 = '#EAECEE';
-export const gray100 = '#F3F4F5';
-export const gray050 = '#F7F8F8';
-export const gray000 = '#FCFCFC';
-export const white = '#FFFFFF';
+export const gray800 = '#3c4144';
+export const gray700 = '#595f63';
+export const gray600 = '#767d82';
+export const gray500 = '#949ba0';
+export const gray400 = '#adb3b8';
+export const gray300 = '#cfd4d7';
+export const gray250 = '#e1e4e6';
+export const gray200 = '#eaecee';
+export const gray100 = '#f3f4f5';
+export const gray050 = '#f7f8f8';
+export const gray000 = '#fcfcfc';
+export const white = '#ffffff';
 
 // Primary
 // Blue
-export const blue800 = '#1B5192';
-export const blue600 = '#2A7DE1';
-export const blue400 = '#6EA7EB';
-export const blue100 = '#D4E5F9';
-export const blue000 = '#F7FAFE';
+export const blue800 = '#1b5192';
+export const blue600 = '#2a7de1';
+export const blue400 = '#6ea7eb';
+export const blue100 = '#d4e5f9';
+export const blue050 = '#e9f2fc';
+export const blue000 = '#f7fafe';
 
 // Success
 // Green
 export const green800 = '#038542';
-export const green600 = '#04CD66';
-export const green400 = '#54DD97';
-export const green100 = '#CDF5E0';
-export const green000 = '#F5FDF9';
+export const green600 = '#04cd66';
+export const green400 = '#54dd97';
+export const green100 = '#cdf5e0';
+export const green050 = '#e5faef';
+export const green000 = '#f5fdf9';
 
 // Error
 // Red
 export const red800 = '#971818';
-export const red600 = '#E92525';
-export const red400 = '#F06B6B';
-export const red100 = '#FBD3D3';
-export const red000 = '#FEF6F6';
+export const red600 = '#e92525';
+export const red400 = '#f06b6b';
+export const red100 = '#fbd3d3';
+export const red050 = '#fde8e8';
+export const red000 = '#fef6f6';
 
 // Secondary
 // Orange
-export const orange800 = '#CF5310';
-export const orange600 = '#EE7E41';
-export const orange400 = '#F3A77E';
-export const orange100 = '#FCE5D9';
-export const orange000 = '#FEFAF7';
+export const orange800 = '#cf5310';
+export const orange600 = '#ee7e41';
+export const orange400 = '#f3a77e';
+export const orange100 = '#fce5d9';
+export const orange050 = '#fdf2eb';
+export const orange000 = '#fefaf7';
 
 // Secondary
 // Yellow
-export const yellow600 = '#FFD700';
-export const yellow400 = '#FFE452';
-export const yellow100 = '#FFF7CC';
-export const yellow000 = '#FFFDF5';
+export const yellow600 = '#ffd700';
+export const yellow400 = '#ffe452';
+export const yellow100 = '#fff7cc';
+export const yellow050 = '#fffbe5';
+export const yellow000 = '#fffdf5';
 
 export const primary = blue600;
 export const lightPrimary = blue100;
@@ -63,56 +68,61 @@ export const error = red600;
 export const info = gray600;
 export const secondary = orange600;
 export const yellow = yellow600;
-export const link = '#006BEB';
+export const link = '#006beb';
 
 export const colors = {
-    black: black,
-    gray900: gray900,
-    gray850: gray850,
-    gray800: gray800,
-    gray700: gray700,
-    gray600: gray600,
-    gray500: gray500,
-    gray400: gray400,
-    gray300: gray300,
-    gray250: gray250,
-    gray200: gray200,
-    gray100: gray100,
-    gray050: gray050,
-    gray000: gray000,
-    white: white,
-    blue800: blue800,
-    blue600: blue600,
-    blue400: blue400,
-    blue100: blue100,
-    blue000: blue000,
-    green800: green800,
-    green600: green600,
-    green400: green400,
-    green100: green100,
-    green000: green000,
-    red800: red800,
-    red600: red600,
-    red400: red400,
-    red100: red100,
-    red000: red000,
-    orange800: orange800,
-    orange600: orange600,
-    orange400: orange400,
-    orange100: orange100,
-    orange000: orange000,
-    yellow600: yellow600,
-    yellow400: yellow400,
-    yellow100: yellow100,
-    yellow000: yellow000,
-    primary: primary,
-    lightPrimary: lightPrimary,
-    success: success,
-    error: error,
-    info: info,
-    secondary: secondary,
-    yellow: yellow,
-    link: link
+	black: black,
+	gray900: gray900,
+	gray850: gray850,
+	gray800: gray800,
+	gray700: gray700,
+	gray600: gray600,
+	gray500: gray500,
+	gray400: gray400,
+	gray300: gray300,
+	gray250: gray250,
+	gray200: gray200,
+	gray100: gray100,
+	gray050: gray050,
+	gray000: gray000,
+	white: white,
+	blue800: blue800,
+	blue600: blue600,
+	blue400: blue400,
+	blue100: blue100,
+	blue050: blue050,
+	blue000: blue000,
+	green800: green800,
+	green600: green600,
+	green400: green400,
+	green100: green100,
+	green050: green050,
+	green000: green000,
+	red800: red800,
+	red600: red600,
+	red400: red400,
+	red100: red100,
+	red050: red050,
+	red000: red000,
+	orange800: orange800,
+	orange600: orange600,
+	orange400: orange400,
+	orange100: orange100,
+	orange050: orange050,
+	orange000: orange000,
+	yellow600: yellow600,
+	yellow400: yellow400,
+	yellow100: yellow100,
+	yellow050: yellow050,
+	yellow000: yellow000,
+	primary: primary,
+	lightPrimary: lightPrimary,
+	success: success,
+	error: error,
+	info: info,
+	secondary: secondary,
+	yellow: yellow,
+	link: link,
 };
 
 export const colorKeys = [...Object.keys(colors)];
