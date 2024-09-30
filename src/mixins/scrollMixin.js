145b6063e16b3476db553ad0c692c4fb5760416c
Donghoon Song <thdehdgns@gmail.com>
2024-09-30 11:31:01 +0900

    chore: 주석 추가

--- src/mixins/scrollMixin.js
+++ src/mixins/scrollMixin.js
@@ -7,12 +7,13 @@ const scrollMixin = {
 				reserveScrollBarGap: true,
 				// iOS에서 이슈가 있어서 사용
 				// https://github.com/willmcpo/body-scroll-lock#allowtouchmove
+				// el이 텍스트 노드나 주석 노드 등 일 경우에 dataset 속성이 없을 수 있음
 				allowTouchMove: el => {
 					while (el && el !== document.body) {
 						if (el instanceof HTMLElement && el.dataset && el.dataset.bodyScrollLockIgnore === 'true') {
 							return true;
 						}
-
+						// parentElement를 사용해 항상 텍스트 노드나 주석 노드 등을 건너뜀
 						el = el.parentElement;
 					}
 					return false;
