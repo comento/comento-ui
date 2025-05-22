18f8891b1aa1e2ce73d44d87ca5298202fa9dc4e
유성실 <yooo1201@naver.com>
2025-05-22 17:50:39 +0900

    feat: EmptyBox illust 추가

---
+++ src/components/emptyBox/EmptyIllust.vue
@@ -0,0 +1,20 @@
+<template>
+	<img :src="`https://cdn.comento.kr/images/illust/empty/${type}.svg`" alt="" width="82" height="82" />
+</template>
+
+<script>
+const ILLUST_SOURCES = ['bookmark', 'class', 'contents', 'credit', 'other', 'payment', 'reply', 'review', 'search'];
+
+export default {
+	name: 'EmptyIllust',
+	props: {
+		type: {
+			type: String,
+			default: 'other',
+			validator: value => ILLUST_SOURCES.includes(value),
+		},
+	},
+};
+</script>
+
+<style scoped lang="scss"></style>
