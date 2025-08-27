cb5034ed8aed020385d39154b9ad76034e60865e
Seongsil Yoo <yooo1201@naver.com>
2025-08-27 11:03:31 +0900

    feat: FileInput 관련 컴포넌트 위계 및 네이밍  변경, 신규 컴포넌트 추가 (#1193)
    * feat: FileList slot으로 변경
    
    * fix: FileItem error일 때 오류 수정
    
    * chore: prettier
    
    * feat: LinkItem 추가
    
    * fix: 첨부파일 개수 수정
    
    * feat: FileInput 관련 컴포넌트 위계, 네이밍 변경
    
    * feat: FileInputList 스토리 추가
    
    * feat: FileSummary 디자인시스템에서 제거
    
    * feat: FileInputLinkItem 하이퍼링크로 변경
    
    * feat: FileInputButton 스토리 보완
    
    * feat: index prop 필수값으로 변경
    
    * refactor: setTimeout으로 변경
    
    * refactor: v-if 말고 style로 처리
    
    * refactor: 이벤트명 변경

---
+++ src/components/fileInput/FileInputList.vue
@@ -0,0 +1,43 @@
+<template>
+	<List class="c-file-input-list--container" :class="{ 'is-empty': !$slots.default, ...computedClass }">
+		<slot />
+	</List>
+</template>
+
+<script>
+import List from '@/components/list/list/List.vue';
+
+/**
+ * @displayName c-file-input-list
+ */
+export default {
+	name: 'FileInputList',
+	props: {
+		full: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	computed: {
+		computedClass() {
+			return { full: this.full };
+		},
+	},
+	components: { List },
+};
+</script>
+
+<style lang="scss" scoped>
+.c-file-input-list--container {
+	border: 1px solid $gray200;
+	@include border-radius(6px);
+	margin-top: 8px;
+	padding: 4px;
+	&.full {
+		width: 100%;
+	}
+	&.is-empty {
+		display: none;
+	}
+}
+</style>
